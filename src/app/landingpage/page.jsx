74c58111b8e1b391e377d1c51aeb791b86314cd2
Semight <davidoluwasemiloorei@gmail.com>
2023-09-15 08:30:29 +0100

    folder structure and installing of depedencies

---
+++ src/app/landingpage/page.jsx
@@ -0,0 +1,12 @@
+import NavBar from "@/public/components/NavBar/NavBar";
+
+const LandingPage = () => {
+    return ( 
+        <>
+        <NavBar />
+        <h1>This is a Landing page</h1>
+        </>
+     );
+}
+ 
+export default LandingPage;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return ( 
+        <>
+        <div className="bg-blue-600 text-center text-white">NavBar</div>
+        </>
+     );
+}
+ 
+export default NavBar;
