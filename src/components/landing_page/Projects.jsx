0681460041575edfdc013394737910cb6342d1aa
Tinalexman <taiwoluwatobilobafestus@gmail.com>
2023-10-03 21:21:27 +0100

    99% done on homepage

--- src/components/landing_page/Projects.jsx
+++ src/components/landing_page/Projects.jsx
@@ -1,4 +1,8 @@
+"use client";
+
 import React from "react";
+
+import { useState } from "react";
 
 import Button from "@/src/components/reusable/Button";
 import { FaArrowRight } from "react-icons/fa";
@@ -11,15 +15,33 @@ import Project2 from "@/public/assets/landing-page/Project 2.png";
 import Project3 from "@/public/assets/landing-page/Project 3.png";
 
 const Projects = () => {
-  const titles = [
-    "UI Design",
-    "Web Development",
-    "Mobile App",
-    "SEO",
-    "Branding",
-  ];
-
   const projects = [Project1, Project2, Project3];
+  const [active, setActive] = useState(0);
+  const selectTab = (index) => setActive(index);
+
+  const children = [
+    <>
+      {active == 0 && (
+        <div key={0} className="flex sm:flex-col gap-5 mt-10 mb-30 items-center px-[10%] sm:px-[5%]">
+          {projects.map((project, i) => {
+            return (
+              <div
+                key={i}
+                className=""
+              >
+                <Image
+                  src={project}
+                  alt="Project Image"
+                  className="sm:w-full"
+                />
+              </div>
+            );
+          })}
+        </div>
+      )}
+      ,
+    </>,
+  ];
 
   return (
     <div className="flex flex-col items-center">
@@ -38,37 +60,98 @@ const Projects = () => {
         </p>
       </div>
 
-      <div className="mt-20 sm:overflow-x-auto sm:w-full sm:px-[5%] flex items-center gap-20">
-        {titles.map((title, i) => {
-          return (
-            <p
-              key={i}
-              className={`opacity-80 cursor-pointer text-center
-              
-              ${
-                i == 0 ? "text-maroon font-medium" : "text-slate-950 font-normal"
-              } 
-              
-              
-              text-base`}
+      <div className="sm:w-full sm:overflow-x-auto scrollbar-custom sm:px-[5%]">
+        <ul
+          className="flex text-sm leading-loose sm:w-full font-medium text-center"
+          id="myTab"
+          data-tabs-toggle="#myTabContent"
+          role="tablist"
+        >
+          <li role="presentation">
+            <button
+              onClick={() => selectTab(0)}
+              className={`inline-block p-4 border-b-2 border-transparent ${
+                active == 0 && "text-maroon border-maroon"
+              } rounded-t-lg hover:text-maroon w-[150px] sm:w-[100px]`}
+              id="design-tab"
+              data-tabs-target="#design"
+              type="button"
+              role="tab"
+              aria-controls="design"
+              aria-selected="true"
             >
-              {title}
-            </p>
-          );
-        })}
-      </div>
-      <div className="flex sm:flex-col gap-10 mt-10 mb-30 items-center px-[10%] sm:px-[5%]">
-        {projects.map((project, i) => {
-          return (
-            <div
-              key={i}
-              className="opacity-80 text-center text-slate-950 text-base font-normal leading-loose"
+              UI Design
+            </button>
+          </li>
+          <li role="presentation">
+            <button
+              onClick={() => selectTab(1)}
+              className={`inline-block p-4 border-b-2 border-transparent ${
+                active == 1 && "text-maroon border-maroon"
+              } rounded-t-lg hover:text-maroon sm:w-[150px] w-[180px]`}
+              id="web-tab"
+              data-tabs-target="#web"
+              type="button"
+              role="tab"
+              aria-controls="web"
+              aria-selected="false"
             >
-              <Image src={project} alt="Project Image" className="sm:w-full" />
-            </div>
-          );
-        })}
+              Web Development
+            </button>
+          </li>
+          <li role="presentation">
+            <button
+              onClick={() => selectTab(2)}
+              className={`inline-block p-4 border-b-2 ${
+                active == 2 && "text-maroon border-maroon"
+              } border-transparent rounded-t-lg hover:text-maroon w-[120px]`}
+              id="mobile-tab"
+              data-tabs-target="#mobile"
+              type="button"
+              role="tab"
+              aria-controls="mobile"
+              aria-selected="false"
+            >
+              Mobile App
+            </button>
+          </li>
+          <li role="presentation">
+            <button
+              onClick={() => selectTab(3)}
+              className={`inline-block p-4 border-b-2 ${
+                active == 3 && "text-maroon border-maroon"
+              } border-transparent rounded-t-lg hover:text-maroon w-[80px]`}
+              id="seo-tab"
+              data-tabs-target="#seo"
+              type="button"
+              role="tab"
+              aria-controls="seo"
+              aria-selected="false"
+            >
+              SEO
+            </button>
+          </li>
+          <li role="presentation" className="pr-5">
+            <button
+              onClick={() => selectTab(4)}
+              className={`inline-block p-4 border-b-2 ${
+                active == 4 && "text-maroon border-maroon"
+              } border-transparent rounded-t-lg hover:text-maroon w-[120px]`}
+              id="brand-tab"
+              data-tabs-target="#brand"
+              type="button"
+              role="tab"
+              aria-controls="brand"
+              aria-selected="false"
+            >
+              Branding
+            </button>
+          </li>
+        </ul>
       </div>
+
+      <div key={active}>{children}</div>
+
       <Button
         style="text-white mt-[32px] bg-darkBlue px-5 py-3 font-[500] rounded-md flex gap-2 items-center hover:bg-extraDarkRed transition ease-in-out duration-200"
         destination={"/our-works"}
